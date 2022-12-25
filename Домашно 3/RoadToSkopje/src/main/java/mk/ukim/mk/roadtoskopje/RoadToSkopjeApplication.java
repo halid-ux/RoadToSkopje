6ee2214049a1b963d52f40ed129a0dd82eab249a
Halid Veaposki <63209157+halid-ux@users.noreply.github.com>
2022-12-25 22:55:28 +0100

    Initialized SpringBoot backend
    Initialized SpringBoot backend
    
    Co-Authored-By: Egzon Ajdini <76227608+egzonajdini@users.noreply.github.com>

---
+++ Домашно 3/RoadToSkopje/src/main/java/mk/ukim/mk/roadtoskopje/RoadToSkopjeApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.mk.roadtoskopje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadToSkopjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadToSkopjeApplication.class, args);
+	}
+
+}
