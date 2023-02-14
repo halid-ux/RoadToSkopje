8e1191c22b1c30612db0100801fb21d9b7950228
Halid Veaposki <63209157+halid-ux@users.noreply.github.com>
2023-02-15 00:18:12 +0100

    Microservices architecture created
    Added Restaurant service and Search service
    
    Co-Authored-By: Egzon Ajdini <76227608+egzonajdini@users.noreply.github.com>
    Co-Authored-By: angelakare <118060674+angelakare@users.noreply.github.com>
    Co-Authored-By: ninssss <86925935+ninssss@users.noreply.github.com>

---
+++ Домашно 4/microservices/restaurantsservice/src/main/java/mk/ukim/finki/skopjeguide/restaurantsservice/RestaurantsserviceApplication.java
@@ -0,0 +1,14 @@
+package mk.ukim.finki.skopjeguide.restaurantsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsserviceApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsserviceApplication.class, args);
+	}
+
+}
